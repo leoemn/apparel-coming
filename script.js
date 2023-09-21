cf0591006aaa7545c90f2d966504dd5a00918f39
Sagar Joshi <91314367+leoemn@users.noreply.github.com>
2023-09-21 17:55:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,15 +6,25 @@ function isValidEmail(email) {
 document.querySelector('.email-button').addEventListener('click', function() {
     const emailInput = document.querySelector('.text-input');
     const errorMessage = document.getElementById('error-message');
+    const errorIcon = document.getElementById("error-icon");
 
     if (!isValidEmail(emailInput.value)) {
         emailInput.style.border = '2px solid hsl(0, 93%, 68%)'; // Change border to red
         errorMessage.textContent = 'Please providre a valid email'; // Set error message
+        emailInput.style.border = '1px solid hsl(0, 93%, 68%)'
         errorMessage.style.display = 'block'; // Display the error message
+        errorIcon.style.display = "block"
     } else {
         emailInput.style.border = '0.5px solid hsl(0, 36%, 70%)'; // Reset border to original
         errorMessage.style.display = 'none'; // Hide error message
+        errorIcon.style.display = "none"
         // Proceed with the next steps, e.g., sending the email to the server.
         console.log('Email is valid');
     }
+
+    emailInput.addEventListener("click", () => {
+        errorIcon.style.display = "none"
+        errorMessage.style.display = 'none';
+        emailInput.style.border = '0.5px solid hsl(0, 36%, 70%)'
+    })
 });
